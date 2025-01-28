13d5b1154049628810522e9e6bcf29e38cb53b45
jan <janj@econsys.co.za>
2025-01-28 08:34:54 +0200

    Busy setting up Theming

---
+++ src/Web/FX850P.Blazor/Constants/MyTheme.cs
@@ -0,0 +1,14 @@
+﻿namespace FX850P.Blazor.Constants;
+
+public static class MyTheme
+{
+    public static partial class Card
+    {
+        public static partial class Header
+        {
+
+        }
+    }
+
+}
+
