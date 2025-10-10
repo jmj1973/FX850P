de35d73a33dcaa18c7c8d1afebe60ba29f17f877
jan <janj@econsys.co.za>
2025-10-10 07:32:39 +0200

    Preformance improvement

--- src/Core/FX850P.Application/Mediator/ApplicationMediator.cs
+++ src/Core/FX850P.Application/Mediator/ApplicationMediator.cs
@@ -7,14 +7,14 @@ using Microsoft.Extensions.DependencyInjection;
 namespace FX850P.Application.Mediator;
 public class ApplicationMediator : IApplicationMediator
 {
-    private readonly IServiceProvider _serviceProvider;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public ApplicationMediator(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+    public ApplicationMediator(IServiceScopeFactory scopeFactory) => _scopeFactory = scopeFactory;
 
 
     public async Task<TResponse> Send<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IApplicationRequest<TResponse>
     {
-        using IServiceScope scope = _serviceProvider.CreateScope();
+        using IServiceScope scope = _scopeFactory.CreateScope();
         IApplicationRequestHandler<TRequest, TResponse> handler = scope.ServiceProvider.GetRequiredService<IApplicationRequestHandler<TRequest, TResponse>>();
         return await handler.Handle(request, cancellationToken);
     }
