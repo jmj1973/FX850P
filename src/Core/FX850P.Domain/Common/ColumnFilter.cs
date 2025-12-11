94878df9ae125089ad3e7be3a891ff45d154646f
jan <janj@econsys.co.za>
2025-12-11 09:47:05 +0200

    Fixed minor issues

--- src/Core/FX850P.Domain/Common/ColumnFilter.cs
+++ src/Core/FX850P.Domain/Common/ColumnFilter.cs
@@ -5,5 +5,5 @@ namespace FX850P.Domain.Common;
 public class ColumnFilter<TType>
 {
     public bool HasValue { get; set; }
-    public Expression<Func<TType, bool>> Expression { get; set; } = default(Expression<Func<TType, bool>>)!;
+    public Expression<Func<TType, bool>> Expression { get; set; } = default!;
 }
--- src/Core/FX850P.Domain/Presistence/Interfaces/IQueryObject.cs
+++ src/Core/FX850P.Domain/Presistence/Interfaces/IQueryObject.cs
@@ -3,7 +3,7 @@ namespace FX850P.Domain.Presistence.Interfaces;
 
 public interface IQueryObject
 {
-    public string SearchString { get; set; }
+    string SearchString { get; set; }
     string SortBy { get; set; }
     bool IsSortAscending { get; set; }
     int Page { get; set; }
